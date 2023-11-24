06fa878b8a383e112aee71773646f6565b039fb4
Ricardo Villanueva <ricardo.villanueva@axisdata.net>
2023-11-24 14:05:23 +0100

    Yo que se

---
+++ BillMonthBack/src/main/java/villanueva/ricardo/BillingApp.java
@@ -0,0 +1,9 @@
+package villanueva.ricardo;
+
+import org.springframework.boot.SpringApplication;
+
+public class BillingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BillingApp.class, args);
+    }
+}
---
+++ BillMonthBack/src/main/java/villanueva/ricardo/UserManagmentResources.java
@@ -0,0 +1,9 @@
+package villanueva.ricardo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserManagmentResources {
+
+
+}
