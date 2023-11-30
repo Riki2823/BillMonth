e2b09e40ad99d94c9528c60c152702d0efb4675b
Ricardo Villanueva <ricardo.villanueva@axisdata.net>
2023-11-30 13:31:18 +0100

    form example

--- bill_month_front/src/routes/Login.tsx
+++ bill_month_front/src/routes/Login.tsx
@@ -1,8 +1,27 @@
 function Login() {
   return (
     <>
-      <form>
-        <input type="emailLogin" name="emailLogin" id="emailLogin" className="form-input px-4 py-3 rounded-full"/>
+      <form className="max-w-md mx-auto m-4 p-6 bg-white rounded-lg shadow-md">
+        <input
+          type="email"
+          name="emailLogin"
+          id="emailLogin"
+          className="w-full px-4 py-3 mb-3 bg-gray-100 rounded-full focus:outline-none focus:ring focus:border-blue-300"
+          placeholder="Correo electrónico"
+        />
+        <input
+          type="password"
+          name="passwordLogin"
+          id="passwordLogin"
+          className="w-full px-4 py-3 mb-3 bg-gray-100 rounded-full focus:outline-none focus:ring focus:border-blue-300"
+          placeholder="Contraseña"
+        />
+        <button
+          type="submit"
+          className="w-full px-4 py-3 bg-blue-500 text-white rounded-full hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+        >
+          Iniciar sesión
+        </button>
       </form>
     </>
   );
